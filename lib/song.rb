dcc6e6ffa3dfe7a9c988e5bcc9d5911cb0bb52c6
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-13 01:12:59 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  
+  def initialize (song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+  end
+  
+  
+    
